77899108fe4a9d023192001d03c9e483d90acedc
Akhil <98938102+AKH1LS@users.noreply.github.com>
2022-05-28 12:54:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,27 +17,27 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 async def start_private(client: Client, message: Message):
  await message.reply_photo(
         photo=f"{START_PIC}",
-        caption=f"""**💥 𝐇𝐞𝐥𝐥𝐨, 𝐈 𝐚𝐦 𝐒𝐮𝐩𝐞𝐫𝐟𝐚𝐬𝐭 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲
-𝐍𝐨 𝐋𝐚𝐠 𝐕𝐂 𝐌𝐮𝐬𝐢𝐜 𝐏𝐥𝐚𝐲𝐞𝐫 𝐁𝐨𝐭 💞 𝐉𝐮𝐬𝐭 𝐀𝐝𝐝 𝐌𝐞 » 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 𝐀𝐧𝐝
-𝐄𝐧𝐣𝐨𝐲 𝐒𝐮𝐩𝐞𝐫 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 ❥︎𝐌𝐮𝐬𝐢𝐜.**""",
+        caption=f"""**Hello, I am **Sparky Music Player**, a lag free with smooth functioning
+music player with awesome features, provide your query, only a minisecond after your command, based on private server.
+If you wanted a unique and better music bot, give my try...**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "➕ 𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 ➕",
+                        "Add me to your group",
                         url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
                     )
                 ],
                 [
-                    InlineKeyboardButton("⭐ 𝐎𝐰𝐧𝐞𝐫'𝐱𝐃 ✨", url=f"https://t.me/{OWNER_USERNAME}"),
-                    InlineKeyboardButton("💐 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫'𝐱𝐃 💥", url=f"https://t.me/Shailendra34"),
+                    InlineKeyboardButton("Creator", url=f"https://t.me/Tera_Baap_Sparky"),
+                    InlineKeyboardButton("Contributer", url=f"https://t.me/AKH1LS"),
                 ],
                 [
                     InlineKeyboardButton(
-                        "💝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐂𝐡𝐚𝐭 💫", url=f"{SUPPORT_GROUP}"
+                        "SUPPORT", url=f"{SUPPORT_GROUP}"
                     ),
                     InlineKeyboardButton(
-                        "🌸 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ✨", url=f"{UPDATES_CHANNEL}"
+                        "CHANNEL", url=f"{UPDATES_CHANNEL}"
                     ),
                 ],
             ]
