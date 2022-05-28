9271bdeedd4b8b9d3168952a22d33c0f2908d32d
Akhil <98938102+AKH1LS@users.noreply.github.com>
2022-05-28 12:48:52 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -106,7 +106,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
     draw.text(
         (190, 670),
-        f"Powered By: HERO",
+        f"Powered By: SPARKY",
         (255, 255, 255),
         font=font,
     )
@@ -134,7 +134,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "Aditya_Player"
+        user.first_name = "rudra_Player"
     usar = user
     wew = usar.id
     try:
