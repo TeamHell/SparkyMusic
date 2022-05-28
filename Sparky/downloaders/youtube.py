dff06d66efedc0202af6dff09cd9d9ef8fc063be
Akhil <98938102+AKH1LS@users.noreply.github.com>
2022-05-28 13:27:47 +0530

    Update youtube.py

--- Sparky/downloaders/youtube.py
+++ Sparky/downloaders/youtube.py
@@ -19,12 +19,12 @@ def download(url: str) -> str:
     duration = round(info["duration"] / 60)
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
+            f"😐 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
         )
     try:
         ydl.download([url])
     except:
         raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
+            f"😐 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
         )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
